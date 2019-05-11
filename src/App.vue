d92432b16bbf9caa3ae8fee51c4d54a2980fa9e4
savaukr <sava_ukr@ukr.net>
2019-05-11 13:56:13 +0300

    beginer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+    <Header></Header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#container {
+    width:100%;
+    height: 100%;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,259 @@
+<template>
+	<div id="header">
+            <div id="logo">
+                    <a href="#"><img src="../../img/logo.png" alt=""></a>
+            </div>
+            <div id="headRight">
+                <div id="dollar" class="headRight">
+                    <nav class="navMenuHor">
+                        <ul>
+                            <li>
+                                <a href="#"><img src="../../img/icon_dollar.png"><span>0.00</span></a>
+                                <img src="../../img/figure7Blue.png">
+                                <ul>
+                                    <li><a href="#">Themes</a></li>
+                                    <li><a href="#">Plugins</a>
+                                        <ul>
+                                            <li><a href="#">Stuff</a></li>
+                                            <li><a href="#">Things</a></li>
+                                            <li><a href="#">Other Stuff</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Tutorials</a>
+                                        <ul>
+                                            <li><a href="#">Stuff</a></li>
+                                            <li><a href="#">Things</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+
+                   <!-- <img src="./img/icon_dollar.png">
+                    <span>0.00 <img src="../../img/figure7Blue.png"></span>-->
+                </div>
+                <div id="lang" class="headRight">
+                    <span>
+                        <select>
+                            <option value="rus">РУС</option>
+                            <option value="eng">ENG</option>
+                            <option value="uk">УКР</option>
+                        </select>
+                    </span>
+                    <img  id = "fig" src="../../img/figure7Blue.png" alt="">
+                        <!--<span>РУС</span>
+                    <img src="../../img/figure7Blue.png">-->
+                </div>
+                <div id="iconMiddle" class="headRight">
+                    <img src="../../img/alarm.png">
+                    <span id="countAlarm">1</span>
+                    <img src="../../img/figure10.png">
+                    <img src="../../img/figure11.png">
+                </div>
+                <div id="account" class="headRight">
+                    <img src="../../img/figure8.png" alt="">
+                    <span id="accountName">Accountname <img src="../../img/figure7.png"></span>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+	export default {
+		name: 'Header',
+	}
+</script>
+
+<style>
+	#header, #basic {
+	   width:100%;
+	   display: flex;
+	   box-sizing: border-box;
+	}
+
+	#logo {
+	    display:block;
+	   width:220px;
+	   height: 75px;
+	   padding: 0 7px;
+	   line-height: 75px;
+	   border-bottom: 1px solid #D8DEE3;
+	   background-color: #F2F4F6;
+	   box-sizing: border-box;
+	   
+	}
+	#headRight {
+	    flex:1;
+	    box-sizing: border-box;
+	} 
+	#logo img {
+	    width: 100%;
+	    height: auto;
+	    vertical-align: middle;
+	}
+	#headRight {
+    height: 75px;
+    background-color: #fff;
+}
+
+#dollar {
+      font-size: 24px;
+      color:#278FFF;
+      float:left;
+      padding-top: 20px;
+      padding-left: 5px;
+}
+.headRight {
+     padding-top: 25px;
+     height: 55px;
+}
+#dollar span, #dollar span img {
+    position: relative;
+    top: -7px;
+}
+
+#account, #iconMiddle {
+    padding-right: 10px;
+    padding-left: 10px;
+    border-right: 1px solid #D8DEE3;   
+    float:right;
+}
+#iconMiddle img:nth-child(3){
+    padding-right: 15px;
+}
+#accountName {
+    position: relative;
+    top: -10px;
+}
+#countAlarm {
+    color: #fff;
+    position: relative;
+    font-size: 10px;
+    top:-20px;
+    left:-19px;
+    border-radius: 10px;
+    padding: 2px 5px;
+    background-color: #E2233C; 
+}
+#lang {
+    font-size: 16px;
+    color: #278FFF;
+    padding-left: 10px;
+    float: right;
+
+}
+
+select{
+-moz-appearance: none;
+-webkit-appearance: none;
+border: none;
+position: relative;
+outline: none;
+padding-right: 20px;
+font-size: 16px;
+color:#278FFF;;
+font-family: "OpenSans-Semibold";
+-webkit-user-select: none;
+-moz-user-select: -moz-none;
+-ms-user-select: none;
+ user-select: none;
+
+
+
+}
+#fig {
+    position: relative;
+    left: -20px;
+    top: -4px;
+
+}
+
+select:after{
+  content: "";
+  display: block;
+  width: 25px;
+  height: 25px;
+  background-image: url("../../img/figure7.png");
+  position: absolute;
+  right: 5px;
+  top: 5px;
+}
+
+/* menu dollar*/
+/*------------------------*/
+.navMenuHor  {
+
+}
+.navMenuHor ul {
+    padding: 0;
+    margin:0;
+    list-style: none;
+    position: relative;
+    z-index: 100;
+
+}
+.navMenuHor ul li {
+    margin: 0 -7px 0 0;
+    display: inline-block;
+    background-color: #fff;
+    padding-left: 5px;
+}
+.navMenuHor ul li  img {
+    
+}
+.navMenuHor a {
+    display: inline-block;
+    color:  #278FFF;
+    font-size: 24px;
+    text-decoration: none;
+}
+.navMenuHor span {
+    padding: 0 7px;
+}
+.navMenuHor li > img {
+    position: relative;
+    top: -12px;
+}
+
+.navMenuHor :hover {
+    background-color: #fff;
+    color: red;/**/
+    z-index: 100;
+
+}
+
+.navMenuHor ul ul {
+    border: 1px solid #D8DEE3;
+    border-radius: 3px;
+    margin-left: -6px;
+    display:none;
+    position: absolute;
+    top:100%;
+}
+.navMenuHor ul li:hover > ul {
+    display: inherit;
+
+}
+svg {
+    z-index: 1;
+}
+
+.navMenuHor ul ul li {
+    padding-left: 5px;
+    min-width: 170px;
+    float:none;
+    display:list-item;
+    position: relative;
+}
+
+/*третій рівень*/
+.navMenuHor ul ul ul {
+    position: absolute;
+    top:0;
+    left:100%;
+}
+/*------------------------*/
+
+
+</style>
